8fcb32558cda393f865ee463a0ebe58a77cea999
maciejgogulski <maciej.gogulski1@gmail.com>
2023-03-11 11:16:49 +0100

    Simple routing

---
+++ src/components/home/OtherPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class OtherPage extends React.Component {
+    render() {
+        return (
+            <h1>OtherPage</h1>
+        );
+    }
+}
+
+export default OtherPage;
