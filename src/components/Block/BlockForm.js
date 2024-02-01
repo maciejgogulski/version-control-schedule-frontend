4d13a78a2206a731e27558bbdbe97306b38bc144
maciejgogulski <maciej.gogulski1@gmail.com>
2024-02-01 14:16:42 +0100

    Attempt to do hook form refactor on BlockForm dynamic fields.

--- src/components/Block/BlockForm.js
+++ src/components/Block/BlockForm.js
@@ -8,6 +8,8 @@ import Parameter from "../../models/Parameter"
 import './BlockForm.css'
 import {useDependencies} from "../../context/Dependencies";
 import {useAuth} from "../../context/Auth";
+import {FormProvider, useFieldArray, useForm} from "react-hook-form";
+import Input from "../Form/Input";
 
 function BlockForm(props) {
     const {t} = useTranslation()
@@ -15,6 +17,24 @@ function BlockForm(props) {
     const {getApiService, getToastUtils} = useDependencies()
     const apiService = getApiService()
     const toastUtils = getToastUtils()
+
+    const form = useForm({
+        defaultValues: {
+            'name': '',
+            'startDate': '',
+            'endDate': '',
+            'newParameterName': '',
+            'newParameterValue': '',
+            'parameters': [],
+            'newParameters': [],
+            'deletedParameters': []
+        }
+    })
+
+    const arrayFields = useFieldArray({
+        ,
+        name: 'parameters'
+    })
 
     const initialState = {
         blockService: apiService.getBlockService(token),
@@ -38,7 +58,7 @@ function BlockForm(props) {
     const fetchParametersForBlock = async () => {
         try {
             const data = await state.blockService.getParameters(props.blockToEdit.id)
-            updateState({parameters: data})
+            form.setValue('parameters', data)
         } catch (error) {
             toastUtils.showToast(
                 'error',
@@ -47,21 +67,20 @@ function BlockForm(props) {
         }
     }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault()
+    const onSubmit = async (data) => {
         try {
             let block = (props.blockToEdit) ? props.blockToEdit : new Block()
             block.scheduleId = props.scheduleId
-            block.name = (props.blockToEdit) ? state.parameters[0].value : state.name
+            block.name = ((props.blockToEdit) ? data.parameters[0].value : data.name).trim()
 
-            block.startDate = parseToServerFormat((props.blockToEdit) ? state.parameters[1].value : state.startDate)
-            block.endDate = parseToServerFormat((props.blockToEdit) ? state.parameters[2].value : state.endDate)
+            block.startDate = parseToServerFormat((props.blockToEdit) ? data.parameters[1].value : data.startDate).trim()
+            block.endDate = parseToServerFormat((props.blockToEdit) ? data.parameters[2].value : data.endDate).trim()
 
             if (block.id) {
-                state.parameters.map(async (parameter) => {
+                data.parameters.map(async (parameter) => {
                     await state.blockService.assignParameterToBlock(parameter)
                 })
-                state.deletedParameters.map(async (parameter) => {
+                data.deletedParameters.map(async (parameter) => {
                     await state.blockService.deleteParameterFromBlock(parameter)
                 })
                 await state.blockService.editBlock(block)
@@ -83,83 +102,85 @@ function BlockForm(props) {
             )
         }
 
-        updateState({
-            name: '',
-            startDate: '',
-            endDate: '',
-            parameters: [],
-            newParameters: [],
-            deletedParameters: []
-        })
-
         props.onClose()
         props.onFormSubmit()
     }
 
     useEffect(() => {
         if (props.blockToEdit) {
-            updateState({
-                name: props.blockToEdit.name,
-                startDate: props.blockToEdit.startDate,
-                endDate: props.blockToEdit.endDate
-            })
+            form.setValue('name', props.blockToEdit.name)
+            form.setValue('startDate', props.blockToEdit.startDate)
+            form.setValue('endDate', props.blockToEdit.endDate)
+
             fetchParametersForBlock()
         } else {
-            updateState({
-                startDate: format(props.pickedDay, "yyyy-MM-dd HH:mm:ss"),
-                endDate: format(props.pickedDay, "yyyy-MM-dd HH:mm:ss")
-            })
+            form.setValue('startDate', format(props.pickedDay, "yyyy-MM-dd HH:mm:ss"))
+            form.setValue('endDate', format(props.pickedDay, "yyyy-MM-dd HH:mm:ss"))
         }
 
-    }, [props.pickedDay, props.blockToEdit])
+    }, [props.pickedDay, props.blockToEdit, form])
 
-    const handleParameterChange = (id, value) => {
-        updateState({
-            parameters: state.parameters.map(parameter => {
-                if (parameter.id === id) {
-                    return {...parameter, value}
-                }
-                return parameter
-            })
-        })
-    }
+    // const handleParameterChange = (id, value) => {
+    //     updateState({
+    //         parameters: state.parameters.map(parameter => {
+    //             if (parameter.id === id) {
+    //                 return {...parameter, value}
+    //             }
+    //             return parameter
+    //         })
+    //     })
+    // }
 
     const handleAddParameterSubmit = async (e) => {
         e.preventDefault()
 
         let newParameter = new Parameter();
         newParameter.blockId = props.blockToEdit.id
-        newParameter.parameterName = state.newParameterName
-        newParameter.value = state.newParameterValue
-        state.newParameters.push(newParameter)
-        state.parameters.push(newParameter)
+        newParameter.parameterName = form.getValues('newParameterName')
+        newParameter.value = form.getValues('newParameterValue')
+        form.setValue(
+            'newParameters',
+            form.getValues('newParameters').push(newParameter)
+        )
+        form.setValue(
+            'parameters',
+            form.getValues('parameters').push(newParameter)
+        )
+
+        form.setValue('newParameterName', '')
+        form.setValue('newParameterValue', '')
 
         updateState({
-            newParameterName: '',
-            newParameterValue: '',
             showAddParameterField: false
         })
     }
 
     const handleParameterDelete = async (parameter) => {
-        const updatedParameters = state.parameters.filter(param => param.parameterName !== parameter.parameterName)
-        updateState({parameters: updatedParameters})
+        const updatedParameters = form.getValues('parameters').filter(param => param.parameterName !== parameter.parameterName)
+        form.setValue('parameters', updatedParameters)
         if (parameter.id) {
-            state.deletedParameters.push(parameter)
+            form.setValue(
+                'deletedParameters',
+                form.getValues('deletedParameters').push(parameter)
+            )
         } else {
-            const updatedNewParameters = state.newParameters.filter(param => param.parameterName !== parameter.parameterName)
-            updateState({newParameters: updatedNewParameters})
+            const updatedNewParameters = form.getValues('newParameters').filter(param => param.parameterName !== parameter.parameterName)
+            form.setValue(
+                'newParameters',
+                form.getValues('newParameters').push(updatedNewParameters)
+            )
         }
     }
 
     const handleFormClose = () => {
+        form.setValue('name', '')
+        form.setValue('startDate', '')
+        form.setValue('endDate', '')
+        form.setValue('parameters', [])
+        form.setValue('newParameters', [])
+
         updateState({
             showAddParameterField: false,
-            name: '',
-            startDate: '',
-            endDate: '',
-            parameters: [],
-            newParameters: []
         })
         props.onClose()
     }
@@ -178,45 +199,56 @@ function BlockForm(props) {
     }
 
     return (
-        <Modal show={props.show} onHide={handleFormClose}>
-            <Modal.Header closeButton>
-                <Modal.Title>{(props.blockToEdit) ? t('entities.block.editing_block') + ": " + props.blockToEdit.name : t('entities.block.creating_new_block')}</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Form onSubmit={handleSubmit}>
-                    {!props.blockToEdit && (
-                        <>
-                            <Form.Group controlId="name">
-                                <Form.Label>{t('entities.block.name')}:</Form.Label>
-                                <Form.Control
-                                    type="text"
-                                    value={state.name}
-                                    onChange={(e) => updateState({name: e.target.value})}
+        <FormProvider {...form}>
+            <Form onSubmit={onSubmit}>
+                <Modal show={props.show} onHide={handleFormClose}>
+                    <Modal.Header closeButton>
+                        <Modal.Title>{(props.blockToEdit) ? t('entities.block.editing_block') + ": " + props.blockToEdit.name : t('entities.block.creating_new_block')}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+
+                        {!props.blockToEdit && (
+                            <>
+                                <Input
+                                    name={'name'}
+                                    type={'text'}
+                                    label={t('entities.block.name')}
+                                    rules={{
+                                        required: t('form.required', {fieldName: `"${t('entities.block.name')}"`})
+                                    }}
                                 />
-                            </Form.Group>
-                            <Form.Group controlId="startDate">
-                                <Form.Label>{t('entities.block.start_date')}:</Form.Label>
-                                <Form.Control
-                                    type="datetime-local"
-                                    value={state.startDate}
-                                    onChange={(e) => updateState({startDate: e.target.value})}
+
+                                <Input
+                                    name={'startDate'}
+                                    type={'datetime-local'}
+                                    label={t('entities.block.start_date')}
+                                    rules={{
+                                        required: t('form.required', {fieldName: `"${t('entities.block.start_date')}"`})
+                                    }}
                                 />
-                            </Form.Group>
-                            <Form.Group controlId="endDate">
-                                <Form.Label>{t('entities.block.end_date')}:</Form.Label>
-                                <Form.Control
-                                    type="datetime-local"
-                                    value={state.endDate}
-                                    onChange={(e) => updateState({endDate: e.target.value})}
+
+                                <Input
+                                    name={'endDate'}
+                                    type={'datetime-local'}
+                                    label={t('entities.block.end_date')}
+                                    rules={{
+                                        required: t('form.required', {fieldName: `"${t('entities.block.end_date')}"`})
+                                    }}
                                 />
-                            </Form.Group>
-                        </>
-                    )}
-                    {state.parameters.map((parameter, index) => (
-                        <Form.Group key={parameter.id} controlId={parameter.parameterName}>
+                            </>
+                        )}
+                        {state.parameters.map((parameter, index) => (
                             <div className="row">
-                                <Form.Label
-                                    className="col-md-6">{translateParameterName(parameter.parameterName)}:</Form.Label>
+                                <Input
+                                    className="col-md-6"
+                                    key={parameter.id}
+                                    name={parameter.parameterName}
+                                    type={(index === 1 || index === 2) ? 'datetime-local' : 'text'}
+                                    label={translateParameterName(parameter.parameterName)}
+                                    rules={{
+                                        required: t('form.required', {fieldName: `"${translateParameterName(parameter.parameterName)}"`})
+                                    }}
+                                />
                                 {index >= 3 && (
                                     <CloseButton
                                         className="col-md-6"
@@ -224,68 +256,67 @@ function BlockForm(props) {
                                     />
                                 )}
                             </div>
-                            <Form.Control
-                                type={(index === 1 || index === 2) ? 'datetime-local' : 'text'}
-                                value={parameter.value}
-                                onChange={(e) => handleParameterChange(parameter.id, e.target.value)}
-                            />
-                        </Form.Group>
-                    ))}
-                    {props.blockToEdit && state.showAddParameterField &&
-                        <>
-                            <Modal.Header>
-                                <Modal.Title>{t('entities.parameter.new')}</Modal.Title>
-                            </Modal.Header>
-                            <Form.Group controlId={'newParameterName'}>
-                                <Form.Label>{t('entities.parameter.name')}:</Form.Label>
-                                <Form.Control
-                                    className="bg-success-light"
-                                    type="text"
-                                    value={state.newParameterName}
-                                    onChange={(e) => updateState({newParameterName: e.target.value})}
+                        ))}
+                        {props.blockToEdit && state.showAddParameterField &&
+                            <>
+                                <Modal.Header>
+                                    <Modal.Title>{t('entities.parameter.new')}</Modal.Title>
+                                </Modal.Header>
+                                <Input
+                                    name={'newParameterName'}
+                                    type={'text'}
+                                    label={t('entities.parameter.name')}
+                                    rules={{
+                                        required: t('form.required', {fieldName: `"${t('entities.parameter.name')}"`})
+                                    }}
                                 />
-                            </Form.Group>
-                            <Form.Group controlId={'newParameterValue'}>
-                                <Form.Label>{t('entities.parameter.value')}:</Form.Label>
-                                <Form.Control
-                                    className="bg-success-light"
-                                    type="text"
-                                    value={state.newParameterValue}
-                                    onChange={(e) => updateState({newParameterValue: e.target.value})}
+
+                                <Input
+                                    name={'newParameterValue'}
+                                    type={'text'}
+                                    label={t('entities.parameter.value')}
+                                    rules={{
+                                        required: t('form.required', {fieldName: `"${t('entities.parameter.value')}"`})
+                                    }}
                                 />
-                            </Form.Group>
-                        </>
-                    }
-                </Form>
-                {state.showAddParameterField ?
-                    <Button
-                        className="mt-3"
-                        variant="success"
-                        onClick={handleAddParameterSubmit}>
-                        {t('buttons.add_parameter')}
-                    </Button>
-                    :
-                    <>
-                        {
-                            props.blockToEdit && <Button
+                            </>
+                        }
+
+                        {state.showAddParameterField ?
+                            <Button
                                 className="mt-3"
-                                variant="secondary"
-                                onClick={() => updateState({showAddParameterField: true})}>
+                                variant="success"
+                                onClick={handleAddParameterSubmit}>
                                 {t('buttons.add_parameter')}
                             </Button>
+                            :
+                            <>
+                                {
+                                    props.blockToEdit &&
+                                    <Button
+                                        className="mt-3"
+                                        variant="secondary"
+                                        onClick={() => updateState({showAddParameterField: true})}>
+                                        {t('buttons.add_parameter')}
+                                    </Button>
+                                }
+                            </>
                         }
-                    </>
-                }
-            </Modal.Body>
-            <Modal.Footer>
-                <Button variant={(props.blockToEdit) ? "primary" : "success"} onClick={handleSubmit}>
-                    {(props.blockToEdit) ? t('buttons.edit_block') : t('buttons.create_block')}
-                </Button>
-                <Button variant="secondary" onClick={handleFormClose}>
-                    {t('buttons.close')}
-                </Button>
-            </Modal.Footer>
-        </Modal>
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button
+                            type="submit"
+                            variant={(props.blockToEdit) ? "primary" : "success"}
+                        >
+                            {(props.blockToEdit) ? t('buttons.edit_block') : t('buttons.create_block')}
+                        </Button>
+                        <Button variant="secondary" onClick={handleFormClose}>
+                            {t('buttons.close')}
+                        </Button>
+                    </Modal.Footer>
+                </Modal>
+            </Form>
+        </FormProvider>
     )
 }
 
